13184de30c13cc1abb5e9ce54d8271b6339227e3
Hari <mohammed.fazil@simplifyvms.com>
2022-08-13 14:30:07 +0530

    initial changes

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Home example')
+    .setDescription('The Home API description')
+    .setVersion('1.0')
+    .addTag('Home')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
+}
+bootstrap();
