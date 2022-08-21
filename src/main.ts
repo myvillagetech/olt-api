754fe466c7e18b639cfc85641a6b4eb720842768
Hari <hari.katakam@simplifyvms.com>
2022-08-21 15:22:24 +0530

    moved prod file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
