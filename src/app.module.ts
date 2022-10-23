24bdb37cb5b6ead66b2891915334fa9a5854c264
Hari <hari.katakam@simplifyvms.com>
2022-10-23 18:48:05 +0530

    payments module with contorller and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { TutorProfileModule } from './tutorProfile/tutorProfile.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { RatingsModule } from './ratings/ratings.module';
 import { MesseagesModule } from './messeages/messages.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { MesseagesModule } from './messeages/messages.module';
     ScheduleModule,
     RatingsModule,
     MesseagesModule,
+    PaymentsModule,
   ],
   controllers: [AppController, CategoryController],
 })
