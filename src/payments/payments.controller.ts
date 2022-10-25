f945b225b4095472151cb6fdc13a22571da0ee2d
Hari <hari.katakam@simplifyvms.com>
2022-10-25 18:40:46 +0530

    get payment details

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Payment } from './payment.dto';
 import { PaymentsService } from './payments.service';
@@ -16,6 +16,22 @@ export class PaymentsController {
             const payment = await this.paymentService.addPayment(payload)
             return response.status(HttpStatus.CREATED).json({
                 message: 'Payment details added sucessfully',
+                payment
+            });
+        } catch (error) {
+            console.log(error);
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Get('/:paymentId')
+    async getPayment(
+        @Res() response, @Param('paymentId') paymentId: string
+    ) {
+        try {
+            const payment = await this.paymentService.getPaymentDetails(paymentId)
+            return response.status(HttpStatus.CREATED).json({
+                message: 'Payment details retrieved sucessfully',
                 payment
             });
         } catch (error) {
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ScheduleService } from 'src/schedule/schedule.service';
 import { MODEL_ENUMS } from 'src/shared/enums/models.enums';
 import { Payment } from './payment.dto';
@@ -8,21 +8,45 @@ import { IPaymentDocument } from './payment.schema';
 
 @Injectable()
 export class PaymentsService {
-    constructor(private readonly scheduleService: ScheduleService, 
+    constructor(private readonly scheduleService: ScheduleService,
         @InjectModel(MODEL_ENUMS.PAYMENT) private paymentModel: Model<IPaymentDocument>) { }
-    
+
     async addPayment(
         payload: Payment
     ) {
         try {
-            console.log(payload);
             const payment = new this.paymentModel(payload);
-            const results=  await payment.save();
-            console.log(results);
-            console.log(results["_id"]);
+            const results = await payment.save();
             await this.scheduleService.updatedPaymentDetails(payload.scheduleIds, results["_id"])
-
             return results;
+        } catch (error) {
+            console.log(error);
+            throw new HttpException(
+                `Something went wrong ... Please try again`,
+                HttpStatus.UNPROCESSABLE_ENTITY
+            );
+        }
+    }
+
+    async getPaymentDetails(
+        paymentId: string
+    ) {
+        console.log(paymentId)
+        try {
+            return this.paymentModel.aggregate([
+                {
+                    $match: { '_id': new Types.ObjectId(paymentId) }
+                },
+                {
+                    $lookup: {
+                        from: "schedules",
+                        localField: "scheduleIds",
+                        foreignField: "_id",
+                        as: "Schedules",
+                    }
+                }
+            ]);
+
         } catch (error) {
             console.log(error);
             throw new HttpException(
