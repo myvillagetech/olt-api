4a13ca2219aa399bd6f50725f657c840a125fc91
Hari <hari.katakam@simplifyvms.com>
2022-10-27 21:39:23 +0530

    getAllMessagedUsers

--- src/messeages/messages.service.ts
+++ src/messeages/messages.service.ts
@@ -21,22 +21,24 @@ export class MessagesService {
     }
 
     async getAllMessagesByFromAndTo(dto: FromAndTo) {
-        const messages = await this.messageModel.find({$or: [{
-            from: new Types.ObjectId(dto.from),
-            to: new Types.ObjectId(dto.to)
-        },
-        {
-            from: new Types.ObjectId(dto.to),
-            to: new Types.ObjectId(dto.from)
-        }]})
-        .sort({createdAt:1}).exec();
+        const messages = await this.messageModel.find({
+            $or: [{
+                from: new Types.ObjectId(dto.from),
+                to: new Types.ObjectId(dto.to)
+            },
+            {
+                from: new Types.ObjectId(dto.to),
+                to: new Types.ObjectId(dto.from)
+            }]
+        })
+            .sort({ createdAt: 1 }).exec();
 
         return messages;
     }
 
     async getAllMessagedUsers(user: string) {
-        return this.messageModel.aggregate([
-            { $match: { $or : [{ to: new Types.ObjectId(user)}, { from: new Types.ObjectId(user)}] } },
+        const results =  await this.messageModel.aggregate([
+            { $match: { $or: [{ to: new Types.ObjectId(user) }, { from: new Types.ObjectId(user) }] } },
             {
                 $lookup: {
                     from: "users",
@@ -44,7 +46,7 @@ export class MessagesService {
                     foreignField: "_id",
                     as: "fromUser",
                     "pipeline": [
-                        { "$project": { "password": 0, "userId": 0}}
+                        { "$project": { "password": 0, "userId": 0 } }
                     ]
                 }
             },
@@ -55,13 +57,57 @@ export class MessagesService {
                     foreignField: "_id",
                     as: "toUser",
                     "pipeline": [
-                        { "$project": { "password": 0, "userId": 0}}
+                        { "$project": { "password": 0, "userId": 0 } }
                     ]
                 }
             },
-            { $group : { _id : "$fromUser", 'messages' : { $push : {message: "$message", from: '$from', to: '$to'}}}}
+            {
+                $facet: {
+                    from: [{ $group : { _id : "$fromUser"}}],
+                    to: [{ $group : { _id : "$toUser"}}],
+                }
+            },
+            // {
+            //     $group: {
+            //         _id: null,
+            //         // from: { $push: "$fromUser" },
+            //         to: { $push: "$toUser" },
+
+            //     }
+            // },
+            // {
+            //     $project: {
+            //         distictValues: {
+            //             $reduce: {
+            //                 input: "$tags",
+            //                 initialValue: [],
+            //                 in: { $setUnion: ["$$value", "$$this"] }
+            //             }
+            //         }
+            //     }
+            // }
+            // { $group : { _id : "$fromUser", 'messages' : { $push : {message: "$message", from: '$from', to: '$to'}}}},
+            // { $group : { _id : "$toUser", 'messages' : { $push : {message: "$message", from: '$from', to: '$to'}}}}
         ]);
 
-        // return users;
+        // return results;
+        // console.log(results.constructor);
+        // return ;
+
+        const from = results[0].from ? results[0].from.map(u => u._id[0]).filter(a => a && a._id.toString() !== user ) : [];
+        const to = results[0].to ? results[0].to.map(u => u._id[0]).filter(a => a && a._id.toString() !== user) : [];
+
+        return [...new Map( [...from, ...to].map(item =>
+            [item['email'], item])).values()];
+        // return {from, to};
+
+        // const result = [];
+
+        
+        // let gruoupResults = [...results[0].from, ...results[0].to[0]];
+        // // return gruoupResults;
+        // // gruoupResults = gruoupResults.map(g => g[0]);
+        // gruoupResults = [...new Set(gruoupResults)]
+        // return gruoupResults;
     }
 }
