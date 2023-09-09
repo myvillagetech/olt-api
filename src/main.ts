8345dab3263eac5807036585727623bc819ca674
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2023-09-09 20:43:12 +0530

    changed port name

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('eduffirm')
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
@@ -23,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 6001);
 }
 bootstrap();
